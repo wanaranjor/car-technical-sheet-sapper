cfeb186870181dbe82a87763fcf5b0aa38a41d15
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2020-03-05 21:44:16 -0500

    update SearchForm datalist

--- src/components/SearchForm.svelte
+++ src/components/SearchForm.svelte
@@ -1,10 +1,11 @@
 <script>
-  // import { onMount } from 'svelte';
   import { fade } from 'svelte/transition';
   import axios from 'axios';
 
   let marcas = [];
-  let checkedPlaca = false;
+  let searchMarca = "";
+  // let marcasResults = "";
+
   let checkedClase = false;
   let checkedMarca = false;
   let checkedLinea = false;
@@ -12,10 +13,25 @@
 
   let selectedMarca = '';
 
-  // onMount(async () => {
-  //   marcas = await getAllMarcas();
-  //   console.log(marcas);
-  // });
+  $: if(checkedMarca) getAllMarcas()
+
+  // $: marcas = marcasResults
+
+  // $: if(searchMarca.length >= 2) {
+  //   getSearchMarcas()
+  // }
+
+  // const getSearchMarcas = async () => {
+  //   try {
+  //     const url = `http://localhost:3001/api/marcas?filter[where][marca][like]=`+ searchMarca + `%`;
+  //     const response = await axios.get(url);
+  //     console.log(url, response.data);
+  //     marcasResults = response.data;
+  //   }
+  //   catch(e) {
+  //       console.error("Error al cargar Marcas:", e );
+  //   }
+  // }
 
   const getAllMarcas = async () => {
     try {
@@ -35,19 +51,15 @@
 </script>
 
 <div class="p-3">
-  
+
   <div class="flex flex-wrap items-center text-white  rounded-t border-l border-t border-r  bg-blue-500 px-4 py-1 font-bold">
     <span class="flex-auto">Consultar Vehículo</span>
-    <label class="px-2">
-      <input type=checkbox bind:checked={checkedPlaca} />
-        Placa
-    </label>
     <label class="px-2">
       <input type=checkbox bind:checked={checkedClase} />
         Clase
     </label>
     <label class="px-2">
-      <input type=checkbox bind:checked={checkedMarca} on:click={getAllMarcas}/>
+      <input type=checkbox bind:checked={checkedMarca} />
         Marca
     </label>
     <label class="px-2">
@@ -58,7 +70,7 @@
       <input type=checkbox bind:checked={checkedModelo} />
         Modelo
     </label>
-    {#if checkedPlaca || checkedClase || checkedMarca || checkedLinea || checkedModelo}
+    {#if checkedClase || checkedMarca || checkedLinea || checkedModelo}
       <div transition:fade class="bg-white hover:bg-yellow-500 text-blue-700 hover:text-white px-2 border border-blue-500 hover:border-yellow-500 rounded">
         <button on:click={handleChecked}>
           Buscar
@@ -68,67 +80,58 @@
   </div>
   <div class="text-black rounded-b px-4 py-2 flex flex-col border">
     <div class="-mx-3 md:flex mb-2">
-      {#if checkedPlaca}
+      {#if checkedClase}
         <div transition:fade class="md:w-1/5 px-3">
-          <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-1" for="placa">
-            Placa
+          <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-1" for="clase">
+            Clase
           </label>
           <input
             class="appearance-none block w-full text-sm leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline py-1 px-4"
-            id="placa"
+            id="clase"
             type="text"
-            placeholder="Placa" />
+            bind:value={searchMarca}
+            placeholder="Clase" />
+            {searchMarca}
         </div>
       {/if}
-      {#if checkedClase}
-      <div transition:fade class="md:w-1/5 px-3">
-        <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-1" for="clase">
-          Clase
-        </label>
-        <input
-          class="appearance-none block w-full text-sm leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline py-1 px-4"
-          id="clase"
-          type="text"          
-          placeholder="Clase" />
-      </div>
-      {/if}
       {#if checkedMarca}
-      <div transition:fade class="md:w-1/5 px-3">
-        <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-1" for="marca">
-          Marca
-        </label>
-        <select bind:value={selectedMarca} class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-1 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="marca">
-          <option></option>
-          {#each marcas as marca}
-            <option value={marca.marca}>{marca.marca}</option>
-          {/each}
-        </select>
-      </div>
+        <div transition:fade class="md:w-1/5 px-3">
+          <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-1" for="marca">
+            Marca
+          </label>
+          <input type="text" list="marcaName" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-1 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          bind:value={selectedMarca}/>
+          <datalist id="marcaName">
+            {#each marcas as marca}
+              <option value={marca.marca}>{marca.marca}</option>
+            {/each}
+          </datalist>
+        </div>
       {/if}
       {#if checkedLinea}
-      <div transition:fade class="md:w-1/5 px-3">
-        <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-1" for="linea">
-          Línea
-        </label>
-        <input
-          class="realtive appearance-none block w-full text-sm leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline py-1 px-4"
-          id="linea"
-          type="text"
-          placeholder="Línea" />
-          <!-- TODO: pendiente lupa de busqueda -->
-      </div>
+        <div transition:fade class="md:w-1/5 px-3">
+          <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-1" for="linea">
+            Línea
+          </label>
+          <input
+            class="realtive appearance-none block w-full text-sm leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline py-1 px-4"
+            id="linea"
+            type="text"
+            placeholder="Línea" />
+            <!-- TODO: pendiente lupa de busqueda -->
+        </div>
       {/if}
       {#if checkedModelo}
-      <div transition:fade class="md:w-1/5 px-3">
-        <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-1" for="modelo">
-          Modelo
-        </label>
-        <input
-          class="appearance-none block w-full text-sm leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline py-1 px-4"
-          id="modelo"
-          type="text"
-          placeholder="Modelo" />
-      </div>
+        <div transition:fade class="md:w-1/5 px-3">
+          <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-1" for="modelo">
+            Modelo
+          </label>
+          <input
+            class="appearance-none block w-full text-sm leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline py-1 px-4"
+            id="modelo"
+            type="text"
+            placeholder="Modelo" />
+        </div>
       {/if}
     </div>
   </div>
